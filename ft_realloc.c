fcee4ee44cb6bfabfea31c96d349362924a01ad6
Kevin Visouthivong <kevisout@paul-f4Ar4s2.clusters.42paris.fr>
2024-12-27 18:13:40 +0100

    libft

---
+++ ft_realloc.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_realloc.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kevisout <kevisout@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/27 18:12:31 by kevisout          #+#    #+#             */
+/*   Updated: 2024/12/27 18:12:59 by kevisout         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_realloc(void *ptr, size_t size)
+{
+	void	*new;
+
+	new = ft_calloc(size, 1);
+	if (!new)
+		return (NULL);
+	if (ptr)
+	{
+		ft_memcpy(new, ptr, size);
+		free(ptr);
+	}
+	return (new);
+}
